4684159c2deafbc03b3e36ad1ac318af59dee41b
hxd <wodenianshao@163.com>
2018-05-08 17:54:43 +0800

    改变组件Cube的颜色
    改变组件Cube的颜色

---
+++ New Unity Project/Assets/ColorControl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<MeshRenderer>().material.color = Color.black;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.A))
+        {
+            gameObject.GetComponent<MeshRenderer>().material.color = Color.blue;
+        }
+        else if(Input.GetKeyDown(KeyCode.W))
+        {
+            gameObject.GetComponent<MeshRenderer>().material.color = Color.cyan;
+        }
+        else if(Input.GetKeyDown(KeyCode.S))
+        {
+            gameObject.GetComponent<MeshRenderer>().material.color = Color.yellow;
+        }
+        else if(Input.GetKeyDown(KeyCode.D))
+        {
+            gameObject.GetComponent<MeshRenderer>().material.color = Color.white;
+        }
+	}
+}
